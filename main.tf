d9c1dd52bb93bc4f72be6864470ad1ea92a7eca2
chichiGithub <c.chichi12@yahoo.co.uk>
2026-03-21 18:24:45 +0100

    Fix S3 bucket name to lowercase

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 variable "bucket_suffix" {
   description = "Unique suffix for bucket name (use your initials)"
   type        = string
-  default     = "cco-124"  # CHANGE THIS to your initials!
+  default     = "cco"  # CHANGE THIS to your initials!
 }
 
 # S3 Bucket - using fixed name to prevent duplicates
